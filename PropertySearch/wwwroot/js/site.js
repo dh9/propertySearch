844eb2a86b2bf33353dcf0437c40fadeeabf5c2e
Dan Harris <danharris@dans-air.home>
2018-08-05 19:02:37 +0100

    tidying js a bit making image url work in production

--- PropertySearch/wwwroot/js/site.js
+++ PropertySearch/wwwroot/js/site.js
@@ -2,15 +2,24 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-$(document).ready(function () {
-    $('.js-property-item').on('click', function () {
-        $.ajax(
-            {
-                url: "https://localhost:5001/Property/Image/" + this.id, 
-                success: function(result)
-                {
-                    document.getElementById("js-property-image").src = result;
-                }
-            });
-    });
-});
+var imageController = function () {
+    var init = function (url) {
+        $(document).ready(function () {
+            $('.js-property-item').on('click', function () {
+                $.ajax(
+                   {
+                       url: url + '/' + this.id, 
+                       success: function(result)
+                       {
+                           document.getElementById("js-property-image").src = result;
+                       }
+                  });
+           });
+       });
+    };
+
+    return {
+        init: init
+    };
+}();
+
